1b18e31ffe270424e7d8df7ba56c15306dcbddcc
Simon Knott <info@simonknott.de>
2025-03-27 19:23:50 +0100

    chore: sse test (#59)

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import { spawn } from 'node:child_process';
+import path from 'node:path';
 import { test, expect } from './fixtures';
 
 test('test tool list', async ({ server, visionServer }) => {
@@ -449,4 +451,27 @@ test('stitched aria frames', async ({ server }) => {
       }],
     },
   }));
+});
+
+test('sse transport', async () => {
+  const cp = spawn('node', [path.join(__dirname, '../cli.js'), '--port', '0'], { stdio: 'pipe' });
+  try {
+    let stdout = '';
+    const url = await new Promise<string>(resolve => cp.stdout?.on('data', data => {
+      stdout += data.toString();
+      const match = stdout.match(/Listening on (http:\/\/.*)/);
+      if (match)
+        resolve(match[1]);
+    }));
+
+    // need dynamic import b/c of some ESM nonsense
+    const { SSEClientTransport } = await import('@modelcontextprotocol/sdk/client/sse.js');
+    const { Client } = await import('@modelcontextprotocol/sdk/client/index.js');
+    const transport = new SSEClientTransport(new URL(url));
+    const client = new Client({ name: 'test', version: '1.0.0' });
+    await client.connect(transport);
+    await client.ping();
+  } finally {
+    cp.kill();
+  }
 });
