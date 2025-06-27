af76bf787fc4a1208f81c466bec6ca0693378c7b
Ani Saghatelyan <ani_s@zealous.tech>
2025-06-28 00:51:40 +0400

    Added final styling

--- src/tools/customAgentTools.ts
+++ src/tools/customAgentTools.ts
@@ -137,19 +137,16 @@ const custom_get_computed_styles = defineTool({
     const computedStyles = await locator.evaluate((element: Element, props?: string[]) => {
       const computedStyle = window.getComputedStyle(element);
       const result: { [key: string]: string } = {};
-
-      console.log("propspropsprops:  ", props)
       if (props) {
         props.forEach(propName => {
           result[propName] = computedStyle[propName as any] || computedStyle.getPropertyValue(propName);
-          console.log("AAAAA: ", propName,   result[propName])
         });
       }
 
       return result;
     }, params.propertyNames);
 
-    console.log("AAAAAAAAAAAAAAAAAaa : ", computedStyles);
+    console.log("Requested Computed Styles : ", computedStyles);
     return {
       code: [`// <internal code to get element styles>`],
       action: async () => {
